a5226c35ba7808e5d76fdab61254a6bf95b3f918
Goran Ferbišek <goran.ferbisek@gmail.com>
2024-12-01 17:00:00 +0100

    Setup Wildfly image with configurations and db driver

--- redeployToDocker.sh
+++ redeployToDocker.sh
@@ -1,3 +1,3 @@
 ./gradlew clean assemble
-docker build -t device-history:v1 .
-docker run -it --rm -p 8080:8080 -p 9990:9990 device-history:v1
+docker build -t device-history:v1 -f docker/wildfly/Dockerfile .
+docker run -d --rm -p 8080:8080 -p 9990:9990 device-history:v1
